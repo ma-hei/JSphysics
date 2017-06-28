44795537bcc5123c9505cd5a7264b4b566093fee
ma-hei <marten_heidemeyer@web.de>
2017-06-27 19:06:27 -0700

    simple force/torque drawing is working

--- engine.js
+++ engine.js
@@ -4,8 +4,8 @@ var RigidBody = function(){
 	this.mass = 10;
 	this.I_body = math.matrix([[1,0,0],[0,1,0],[0,0,1]]);
 	this.I_body_inv = math.matrix([[1,0,0],[0,1,0],[0,0,1]]);	
-	this.x = [0,0,1];
-	this.q = [0,0,0,0];
+	this.x = [20,20,0];
+	this.q = [0,0,0,1];
 	this.P = [0,0,0];
 	this.L = [0,0,0];
 	this.I_inv = math.matrix([[1,0,0],[0,1,0],[0,0,1]]);
@@ -20,7 +20,7 @@ var RigidBody = function(){
 var Simulation = function(){
 
 	this.n_bodies = 1;
-	this.state_size = 18;
+	this.state_size = 13;
 	this.rigid_bodies = new Array(this.n_bodies);
 	
 }
@@ -61,10 +61,10 @@ Simulation.prototype.state_to_array = function(rb, y, idx){
 	y[idx+1] = rb.x[1];
 	y[idx+2] = rb.x[2];
 
-	y[idx+3] = rb.q.r;
-	y[idx+4] = rb.q.i;
-	y[idx+5] = rb.q.j;
-	y[idx+6] = rb.q.k;
+	y[idx+3] = rb.q[0];
+	y[idx+4] = rb.q[1];
+	y[idx+5] = rb.q[2];
+	y[idx+6] = rb.q[3];
 
 	y[idx+7] = rb.P[0];
 	y[idx+8] = rb.P[1];
@@ -75,16 +75,27 @@ Simulation.prototype.state_to_array = function(rb, y, idx){
 	y[idx+12] = rb.L[2];
 }
 
+Simulation.prototype.normalize_quaterion = function(q){
+  
+	var s = q[0];
+	var vx = q[1];
+	var vy = q[2];
+	var vz = q[3];
+
+	var length = math.sqrt(s*s+vx*vx+vy*vy+vz*vz);
+	return([s/length,vx/length,vy/length,vz/length]);
+}
+
 Simulation.prototype.array_to_state = function(rb, y, idx){
 
 	rb.x[0] = y[idx+0];
 	rb.x[1] = y[idx+1];
 	rb.x[2] = y[idx+2];
 
-	rb.q.r = y[idx+3];
-	rb.q.i = y[idx+4];
-	rb.q.j = y[idx+5];
-	rb.q.k = y[idx+6];
+	rb.q[0] = y[idx+3];
+	rb.q[1] = y[idx+4];
+	rb.q[2] = y[idx+5];
+	rb.q[3] = y[idx+6];
 
 	rb.P[0] = y[idx+7];
 	rb.P[1] = y[idx+8];
@@ -98,7 +109,7 @@ Simulation.prototype.array_to_state = function(rb, y, idx){
 	rb.v[1] = rb.P[1]/rb.mass;
 	rb.v[2] = rb.P[2]/rb.mass;
 
-	rb.R = this.quaterion_to_matrix(rb.q);
+	rb.R = this.quaterion_to_matrix(this.normalize_quaterion(rb.q));
 	
 	rb.I_inv = math.multiply(math.multiply(rb.R,rb.I_body_inv), math.transpose(rb.R));
 
@@ -119,12 +130,30 @@ Simulation.prototype.array_to_bodies = function(x){
 	}
 }
 
-Simulation.prototype.quaternion_product(a,b){
+Simulation.prototype.quaternion_product = function(a,b){
 	//[s1,v1] * [s2,v2] = s1s2 - v1.v2, s1v2+s2v1 + v1xv2]
+	var s1 = a[0];
+	var v1 = [a[1],a[2],a[3]];
+	var s2 = b[0];
+	var v2 = [b[1],b[2],b[3]];
+
+	var rs = s1*s2-this.dot_product(v1,v2);
+	var rv = [0,0,0];
 	
+	var temp = this.cross_product(v1,v2);
+	
+	rv[0] = s1*v2[0] + s2*v1[0] + temp[0];	
+	rv[1] = s1*v2[1] + s2*v1[1] + temp[1];
+	rv[2] = s1*v2[2] + s2*v1[2] + temp[2];
+	
+	return [rs,rv[0],rv[1],rv[2]];
 }
 
-Simulation.prototype.cross_product(a,b){
+Simulation.prototype.dot_product = function(a,b){
+  return a[0]*b[0]+a[1]*b[1]+a[2]*b[2];
+}
+
+Simulation.prototype.cross_product = function(a,b){
 
   var a1 = a[0];
   var a2 = a[1];
@@ -139,52 +168,144 @@ Simulation.prototype.cross_product(a,b){
 
 }
 
-Simulation.prototype.compute_force_and_torque(rb){
+Simulation.prototype.compute_force_and_torque = function(t, rb){
 
   var f = [1,1,0];
   var r = new Array(3);
-  r[0] = 0.2;
-  r[1] = 0.2;
+  r[0] = 0.002;
+  r[1] = 0.003;
   r[2] = 0;
   
   rb.torque = this.cross_product(r,f);   
   
 }
 
-Simulation.prototype.DdtStateToArray(rb, xdot, idx){
+Simulation.prototype.DdtStateToArray = function(rb, xdot, idx){
+
 	xdot[idx+0] = rb.v[0];
 	xdot[idx+1] = rb.v[1];
 	xdot[idx+2] = rb.v[2];
+	
+	var temp = [0,math.subset(rb.omega, math.index(0)), math.subset(rb.omega, math.index(1)), math.subset(rb.omega, math.index(2)) ];
+	var qdot = this.quaternion_product(temp, rb.q);
+	qdot[0] = 0.5*qdot[0];
+	qdot[1] = 0.5*qdot[1];
+	qdot[2] = 0.5*qdot[2];
+	qdot[3] = 0.5*qdot[3];
 
+	xdot[idx+3] = qdot[0];
+	xdot[idx+4] = qdot[1];
+	xdot[idx+5] = qdot[2];
+	xdot[idx+6] = qdot[3];
+
+	xdot[idx+7] = rb.force[0];
+	xdot[idx+8] = rb.force[1];
+	xdot[idx+9] = rb.force[2];
+
+	xdot[idx+10] = rb.torque[0];
+	xdot[idx+11] = rb.torque[1];
+	xdot[idx+12] = rb.torque[2];
 	
 }
 
-Simulation.prototype.Dxdt(t, x, xdot){
+Simulation.prototype.Dxdt = function(t, x, xdot){
 
 	this.array_to_bodies(x);
 	for (var i=0; i<this.n_bodies; i++){
 		this.compute_force_and_torque(t, this.rigid_bodies[i]);
+		this.DdtStateToArray(this.rigid_bodies[i], xdot, i*this.state_size);
 	}
 
 }
 
-Simulation.prototype.ode(x0, xFinal){
+Simulation.prototype.ode = function(x0, xFinal){
+
+  xdot =  new Array(this.n_bodies * this.state_size);
+  this.Dxdt(0, x0, xdot);
+  for (var i=0;i<this.n_bodies * this.state_size; i++){
+	xFinal[i] = x0[i] + xdot[i];	
+  }
+}
+
+Simulation.prototype.draw = function(callback){
+  var l1 = document.getElementById("l1");
+  var l2 = document.getElementById("l2");
+  var l3 = document.getElementById("l3");
+  var l4 = document.getElementById("l4");
+
+  var x1 = this.rigid_bodies[0].x[0];
+  var y1 = this.rigid_bodies[0].x[1];
+  
+var w = 30;
+  var x_1 = -w/2;
+  var y_1 = -w/2;
+
+  var x_2 = w/2;
+  var y_2 = -w/2;
+
+  var x_3 = w/2;
+  var y_3 = w/2;
+
+  var x_4 = -w/2;
+  var y_4 = w/2;
+
+
+  temp = math.matrix([[x_1,y_1,0]]);
+  res = math.multiply(temp, this.rigid_bodies[0].R);
+
+  l1.setAttribute("x1",  math.subset(res, math.index(0,0))+x1);
+  l1.setAttribute("y1",  math.subset(res, math.index(0,1))+y1);
+
+  l4.setAttribute("x2",  math.subset(res, math.index(0,0))+x1);
+  l4.setAttribute("y2",  math.subset(res, math.index(0,1))+y1);
+
+  temp = math.matrix([[x_2,y_2,0]]);
+  res = math.multiply(temp, this.rigid_bodies[0].R);
+
+  l1.setAttribute("x2",  math.subset(res, math.index(0,0))+x1);
+  l1.setAttribute("y2",  math.subset(res, math.index(0,1))+y1);
+ 
+  l2.setAttribute("x1",  math.subset(res, math.index(0,0))+x1);
+  l2.setAttribute("y1",  math.subset(res, math.index(0,1))+y1);
+
+  temp = math.matrix([[x_3,y_3,0]]);
+  res = math.multiply(temp, this.rigid_bodies[0].R);
+ 
+  l2.setAttribute("x2",  math.subset(res, math.index(0,0))+x1);
+  l2.setAttribute("y2",  math.subset(res, math.index(0,1))+y1);
+ 
+  l3.setAttribute("x1",  math.subset(res, math.index(0,0))+x1);
+  l3.setAttribute("y1",  math.subset(res, math.index(0,1))+y1);
+
+  temp = math.matrix([[x_4,y_4,0]]);
+  res = math.multiply(temp, this.rigid_bodies[0].R);
+   
+  l3.setAttribute("x2",  math.subset(res, math.index(0,0))+x1);
+  l3.setAttribute("y2",  math.subset(res, math.index(0,1))+y1);
+ 
+  l4.setAttribute("x1",  math.subset(res, math.index(0,0))+x1);
+  l4.setAttribute("y1",  math.subset(res, math.index(0,1))+y1);
+  
+}
+
+Simulation.prototype.make_step = function(){
+	for (var i=0;i<this.state_size*this.n_bodies;i++){
+		this.x0[i] = this.xFinal[i];
+	}
+	this.ode(this.x0, this.xFinal);
+	this.array_to_bodies(this.xFinal);
+	this.draw();
+	setTimeout(function(simulation){ simulation.make_step()}, 10, this);
 }
 
 Simulation.prototype.run_simulation = function(){
 
-	var x0 = new Array(this.n_bodies * this.state_size);
-	var xFinal = new Array(this.n_bodies * this.state_size);
+  this.x0 = new Array(this.n_bodies * this.state_size);
+  this.xFinal = new Array(this.n_bodies * this.state_size);
 
-	this.init_states();
-	this.bodies_to_array(xFinal);
-	for (var t=0; t<10.0; t+=1/24){
-		for (var i=0;i<this.state_size*this.n_bodies;i++){
-			x0[i] = xFinal[i];
-		}
-		//this.ode(x0, xFinal, this.state_size*this.n_bodies, t, t+1/
-		this.ode(x0, xFinal);
-		this.array_to_bodies(xFinal);
-	}
+  this.init_states();
+  this.bodies_to_array(this.xFinal);
+
+  this.make_step();
+  
 }
-
