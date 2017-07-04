5956496632d0fd4130aa91c6b93d23dfdff889f4
ma-hei <marten_heidemeyer@web.de>
2017-07-03 19:26:59 -0700

    back to euler step

--- engine.js
+++ engine.js
@@ -1,7 +1,7 @@
 
 var RigidBody = function(){
 	
-	this.mass = 10;
+	this.mass = 1;
 	this.I_body = math.matrix([[1,0,0],[0,1,0],[0,0,1]]);
 	this.I_body_inv = math.matrix([[1,0,0],[0,1,0],[0,0,1]]);	
 	this.x = [100,100,0];
@@ -22,8 +22,10 @@ var Simulation = function(){
 	this.n_bodies = 1;
 	this.state_size = 13;
 	this.rigid_bodies = new Array(this.n_bodies);
-	this.stepsize = 1;
-	this.allowed_error = 0.0001;
+	this.stepsize = 1/24;
+	this.time_step = 1/24;
+	this.allowed_error = 0.001;
+	this.steps_taken = 0;
 }
 
 Simulation.prototype.quaterion_to_matrix = function(q){
@@ -171,20 +173,20 @@ Simulation.prototype.cross_product = function(a,b){
 
 Simulation.prototype.compute_force_and_torque = function(t, rb){
 
-  if (t<2){
-  	var f = [0.1,0.1,0];
+  if (t<(18/24)){
+  	var f = [0,5,0];
   	var r = new Array(3);
-  	r[0] = 0.02;
-  	r[1] = 0.03;
+  	r[0] = 1;
+  	r[1] = 0;
   	r[2] = 0;
 
   	rb.torque = this.cross_product(r,f);   
 	rb.force = f;
   	return;
+  } else {
+  	rb.force=[0,0,0];
+  	rb.torque=[0,0,0];
   }
-  rb.force=[0,0,0];
-  rb.torque=[0,0,0];
-  
   
 }
 
@@ -227,7 +229,6 @@ Simulation.prototype.Dxdt = function(t, x, xdot){
 }
 
 Simulation.prototype.adapt_stepsize = function(x0, t){
-
 	temp = new Array(this.n_bodies * this.state_size);
 	this.Dxdt(t, x0, temp);
 	a = new Array(this.n_bodies * this.state_size);
@@ -240,58 +241,91 @@ Simulation.prototype.adapt_stepsize = function(x0, t){
 		b[i] = x0[i]+0.5*this.stepsize*temp[i];
 	}
 
-	this.Dxdt(t, b, temp);
+	this.Dxdt(t+(this.stepsize), b, temp);
 	for (var i=0;i<this.n_bodies * this.state_size; i++){
 		b[i] = b[i]+0.5*this.stepsize*temp[i];
 	}
 
 	var error = 0;
 	for (var i=0;i<this.n_bodies * this.state_size; i++){
-		error = error + Math.abs(a[i]-b[i]);
+		error = error + (a[i]-b[i])*(a[i]-b[i]);
 	}
 	error = Math.sqrt(error);
-	console.log("current error: "+error);
+	console.log("error: "+error);
 	var new_stepsize = Math.sqrt(this.allowed_error/error) * this.stepsize;
-	console.log("new stepsize: " + 	new_stepsize);
-	this.stepsize = new_stepsize;
+	this.stepsize = Math.min(this.time_step,new_stepsize);
+}
+
+Simulation.prototype.euler_step = function (x0, xFinal, t, t_end){
+	xdot = new Array(this.n_bodies * this.state_size);
+	this.Dxdt(t, x0, xdot);
+	for (var i=0;i<this.n_bodies*this.state_size;i++){
+		xFinal[i] = x0[i] + this.stepsize*xdot[i];
+	}
+}
+
+Simulation.prototype.euler_step_2 = function(x0, xFinal, t, t_end){
+	var current_time = t;
+	console.log(this.stepsize);
+	for (var i=0;i<this.n_bodies*this.state_size;i++){
+		xFinal[i] = x0[i];
+	}
+	while(current_time<t_end){
+		this.adapt_stepsize(x0,t);
+		var step = Math.min(t_end-current_time, this.stepsize);
+		xdot = new Array(this.n_bodies * this.state_size);
+		this.Dxdt(t, x0, xdot);
+		for (var i=0;i<this.n_bodies*this.state_size;i++){
+			xFinal[i] = xFinal[i] + step*xdot[i];
+		}
+		current_time = current_time + step;
+	}
+}
+
+Simulation.prototype.runge_katta = function(){
+
+	var current_time = t;
+	while(current_time<t_end){
+		var step = Math.min(this.stepsize, t_end-current_time);
+		
+		temp = new Array(this.n_bodies * this.state_size);
+		k1 =  new Array(this.n_bodies * this.state_size);
+  		this.Dxdt(current_time, x0, k1);
+
+  		for (var i=0;i<this.n_bodies * this.state_size; i++){
+	  		temp[i] = x0[i] + step*(k1[i]/2);
+  		}  
+  		k2 = new Array(this.n_bodies * this.state_size);
+  		this.Dxdt(current_time, temp, k2);
+
+  		for (var i=0;i<this.n_bodies * this.state_size;i++){
+	  		temp[i] = x0[i] + step*(k2[i]/2);
+  		}
+  		k3 = new Array(this.n_bodies * this.state_size);
+  		this.Dxdt(current_time, temp, k3);
+
+  		for (var i=0;i<this.n_bodies * this.state_sze;i++){
+	  		temp[i] = x0[i]+step*(k3[i]);
+  		}
+  		k4 = new Array(this.n_bodies * this.state_size);
+  		this.Dxdt(current_time, temp, k4);
+
+  		for (var i=0;i<this.n_bodies * this.state_size; i++){
+			xFinal[i] = x0[i] + (1/6)*k1[i] + (1/3)*k2[i] + (1/3)*k3[i] + step*(1/6)*k4[i];
+  		}
+
+  		for (var i=0;i<this.n_bodies * this.state_size; i++){
+			x0[i] = xFinal[i];
+  		}
+
+ 		current_time+=step; 
+  	}
+
 }
 
 Simulation.prototype.ode = function(x0, xFinal,t, t_end){
 
-	this.adapt_stepsize(x0, t);
-
-	var current_time = t;
-	while(current_time<t_end){
-	
-	var step = Math.min(this.stepsize, t_end-current_time);
-		
-  temp = new Array(this.n_bodies * this.state_size);
-  k1 =  new Array(this.n_bodies * this.state_size);
-  this.Dxdt(t, x0, k1);
-
-  for (var i=0;i<this.n_bodies * this.state_size; i++){
-	  temp[i] = x0[i] + step*(k1[i]/2);
-  }  
-  k2 = new Array(this.n_bodies * this.state_size);
-  this.Dxdt(t, temp, k2);
-
-  for (var i=0;i<this.n_bodies * this.state_size;i++){
-	  temp[i] = x0[i] + step*(k2[i]/2);
-  }
-  k3 = new Array(this.n_bodies * this.state_size);
-  this.Dxdt(t, temp, k3);
-
-  for (var i=0;i<this.n_bodies * this.state_sze;i++){
-	  temp[i] = x0[i]+step*(k3[i]);
-  }
-  k4 = new Array(this.n_bodies * this.state_size);
-  this.Dxdt(t, temp, k4);
-
-  for (var i=0;i<this.n_bodies * this.state_size; i++){
-	xFinal[i] = x0[i] + (1/6)*k1[i] + (1/3)*k2[i] + (1/3)*k3[i] + step*(1/6)*k4[i];
-  }
- 	current_time+=step; 
-  }
+	this.euler_step_2(x0, xFinal, t, t+this.time_step);
 }
 
 Simulation.prototype.draw = function(callback){
@@ -359,10 +393,11 @@ Simulation.prototype.make_step = function(t){
 	for (var i=0;i<this.state_size*this.n_bodies;i++){
 		this.x0[i] = this.xFinal[i];
 	}
-	this.ode(this.x0, this.xFinal, t, t+(1/24));
+	this.ode(this.x0, this.xFinal, t, t+simulation.time_step);
 	this.array_to_bodies(this.xFinal);
 	this.draw();
-	setTimeout(function(simulation,t){ simulation.make_step(t)}, 1000/24, this,t+(1/24));
+	this.steps_taken = this.steps_taken+1;
+	setTimeout(function(simulation,t){ simulation.make_step(t)}, simulation.time_step*1000, this,t+simulation.time_step);
 }
 
 Simulation.prototype.run_simulation = function(){
